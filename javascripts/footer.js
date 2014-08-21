ff5c5344b51c43490feca9649073abcfc83ca8f5
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2014-08-21 22:09:21 +0200

    install grunt concat

---
+++ javascripts/footer.js
@@ -0,0 +1,17 @@
+/*!
+ * alienlebarge-blog v1.5.0 (http://www.alienlebarge.ch)
+ * Copyright 2014-2014 Cédric Aellen
+ * Licensed under MIT (https://github.com/alienlebarge/alienlebarge.github.com/blob/master/LICENSE)
+ */
+// Grab a handle to IconicJS and make some settings
+var iconic = IconicJS({
+  pngFallback: 'http://www.alienlebarge.ch/assets/iconic/png'
+});
+;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-51080216-1', 'alienlebarge.ch');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
