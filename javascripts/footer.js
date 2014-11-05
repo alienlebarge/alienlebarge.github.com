f366e71a00607686465797f131c472ff172d9215
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2014-11-05 22:31:54 +0100

    update Google Analytics tracking code
    fix #67

--- javascripts/footer.js
+++ javascripts/footer.js
@@ -12,6 +12,5 @@ var iconic = IconicJS({
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-51080216-1', 'alienlebarge.ch');
-ga('require', 'displayfeatures');
+ga('create', 'UA-51080216-1', 'auto');
 ga('send', 'pageview');
