05a73202690346146e0c13fc4147fe3aa8744a3f
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2014-12-12 13:52:31 +0100

    Revert "style for circle images"
    This reverts commit 93c004a127d4a4f65df22cf12d8551b10a0951ab.

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,16 +30,6 @@ module.exports = function (grunt) {
         },
           files: {
             'stylesheets/style.css': 'stylesheets/style.scss'
-
-        }
-      },
-      hologram: {
-        options: {
-          style: 'expanded'
-        },
-        files: {
-          '_hologram/_css/style.css': 'stylesheets/style.scss'
-
         }
       }
     },
