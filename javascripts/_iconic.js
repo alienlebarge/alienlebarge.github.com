b0797a6b2362359166d4ce73c9306c97c9ab0b77
Cédric Aellen <cedric.aellen@alienlebarge.ch>
2015-02-19 17:52:52 +0100

    disable png fallback

--- javascripts/_iconic.js
+++ javascripts/_iconic.js
@@ -1,4 +1,4 @@
-// Grab a handle to IconicJS and make some settings
-var iconic = IconicJS({
-  pngFallback: 'https://dlgjp9x71cipk.cloudfront.net/assets/iconic/1.7.0/png/black'
-});
+// // Grab a handle to IconicJS and make some settings
+//var iconic = IconicJS({
+//  pngFallback: 'https://dlgjp9x71cipk.cloudfront.net/assets/iconic/1.7.0/png/black'
+//});
--- javascripts/footer.js
+++ javascripts/footer.js
@@ -79,10 +79,10 @@ $(document).ready(function(){
   // Target your .container, .wrapper, .post, etc.
   $(".container").fitVids();
 });
-;// Grab a handle to IconicJS and make some settings
-var iconic = IconicJS({
-  pngFallback: 'https://dlgjp9x71cipk.cloudfront.net/assets/iconic/1.7.0/png/black'
-});
+;// // Grab a handle to IconicJS and make some settings
+//var iconic = IconicJS({
+//  pngFallback: 'https://dlgjp9x71cipk.cloudfront.net/assets/iconic/1.7.0/png/black'
+//});
 ;(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
