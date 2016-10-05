c189acf0d5270bdf83a9b019b45017a8d0db8075
Cédric Aellen <cedric.aellen@vd.ch>
2016-10-05 15:47:44 +0200

    use browsersync to view localhost

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
-var gulp  = require('gulp')
-var shell = require('gulp-shell')
+var gulp  = require('gulp');
+var shell = require('gulp-shell');
+var browserSync = require('browser-sync').create();
 
 gulp.task('jekyll', shell.task([
   'bundle exec jekyll build'
 ]))
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['jekyll'], function() {
+
+    browserSync.init({
+        server: "./_site"
+    });
+
+    gulp.watch(["*.html", "_posts/*.md"], ['jekyll']);
+    gulp.watch("_site/*.html").on('change', browserSync.reload);
+});
 
 gulp.task('default', ['jekyll']);
